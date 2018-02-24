d5bc813658bbaf79930ce510c1ce980cf1969dfa
jaankhan <36411965+jaankhan@users.noreply.github.com>
2018-02-24 21:37:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/server-one', function (req, res) {
+  res.send('Articel 1 is requested');
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
