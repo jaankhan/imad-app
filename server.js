448355d24077f495527e17e62a2099ae7c500519
jaankhan <36411965+jaankhan@users.noreply.github.com>
2018-03-08 21:12:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,16 +12,6 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-var counter = 0 ;
-app.get('/server', function (req, res) {
-  counter = counter + 1;
-  res.send(counter.toString());
-});
-app.get('/account-one', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'account-one.html'));
-});
-
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
