e3650b75135ecf3cfe37ae68b63e656f58da47e4
jaankhan <36411965+jaankhan@users.noreply.github.com>
2018-03-14 01:49:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,10 +90,6 @@ app.get('/article-three', function (req, res) {
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/madi.png', function (req, res) {
