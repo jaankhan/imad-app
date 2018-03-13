303666c63242544a32f7794121d573a0da11319d
jaankhan <36411965+jaankhan@users.noreply.github.com>
2018-03-14 03:26:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 alert('Hi all');
 var element = getElementById('main-text');
 element.innerHTML = 'This is new line';
+var img = getElementById('madi');
+img.onclick = function(){
+    
+};
