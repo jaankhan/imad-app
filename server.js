979197d43e0074e2e4a62c7a29e0402f9f9756cc
jaankhan <36411965+jaankhan@users.noreply.github.com>
2018-03-18 03:27:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var names=[];
 app.get('/submit-name/:name', function(req, res){
    var name=req.params.name;
    names.push(name);
-   req.send(JSON.stringify(names));
+   res.send(JSON.stringify(names));
 });
 /*
 var articles={
